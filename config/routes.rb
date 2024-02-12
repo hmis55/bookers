f0bbd9333d342533b0294988ad2854e0882ef90c
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-02-12 15:54:16 +0000

    [Add] index画面の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'book/new'
+
+  get 'book/index'
+  get 'book/show'
+  get 'book/edit'
+  get '/top' =>'home#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
