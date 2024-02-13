1dd959efde3a3a0e1f015bcd854c2ece8c4681d6
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-02-13 07:36:45 +0000

    routes修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'book/new'
-post 'books' =>'books#create'
-  get 'book/index'
-  get 'book/show'
-  get 'book/edit'
+#   get 'books/new'
+# post 'books' =>'books#create'
+  get 'books/index' => 'books#index'
+  get 'books/show'
+  get 'books/edit'
   get '/top' =>'home#top'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
