abfc5d4b734ff0cf9ec48fd5731cf0ef67875081
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-02-15 06:48:43 +0000

    index,edit エラー分修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,17 +4,18 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
       flash[:notice] = "投稿が成功しました"
-      redirect_to books_index_path
+      redirect_to books_path
     else
-      render :new
+      @books = Book.all
+      render :index
     end
   end
 
   def index
-    @books= Book.all
+    @books = Book.all
     @book = Book.new
   end
 
@@ -28,8 +29,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+   if @book.update(book_params)
+      flash[:notice] = "アップデートに成功しました"
+      redirect_to book_path(@book.id)
+   else
+      render :edit
+   end  
   end
 
   def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
-  get 'books/new'
-  post 'books' =>'books#create'
-  get 'books/index' => 'books#index'
-  get "books/:id" =>'books#show',as: 'book'
-  get "books/:id/edit" =>'books#edit',as: 'edit_book'
-  get '/top' =>'home#top'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
+
+  resources :books
+  get '/top' =>'homes#top'
 end
